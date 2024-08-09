99202fe2a0b48c8f9fe01cb09806e23b77ba7784
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2024-08-09 09:32:12 +0200

    require login on admin page

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import { ThinContainer } from "@/components/layout/ThinContainer";
 import { Heading1, Paragraph } from "@/components/utils/Text";
 import { SubPageLayout } from "@/pages/layouts/SubPageLayout";
@@ -5,10 +7,17 @@ import { AccountCreatePart } from "@/pages/parts/auth/AccountCreatePart";
 import { ConfigValuesPart } from "@/pages/parts/admin/ConfigValuesPart";
 import { TMDBTestPart } from "@/pages/parts/admin/TMDBTestPart";
 import { WorkerTestPart } from "@/pages/parts/admin/WorkerTestPart";
+import { useAuth } from "@/hooks/auth/useAuth";
 
 import { BackendTestPart } from "../parts/admin/BackendTestPart";
 
 export function AdminPage() {
+  const navigate = useNavigate();
+  const { loggedIn } = useAuth();
+  if (!loggedIn){
+    navigate("/login");
+  }
+
   return (
     <SubPageLayout>
       <ThinContainer>
