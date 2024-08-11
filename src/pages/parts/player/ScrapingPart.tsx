4f8eddd702aea74fc3c4491fdc2b456317714472
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2024-08-11 22:11:45 +0200

    Steal from cinewhirl

--- src/pages/parts/player/ScrapingPart.tsx
+++ src/pages/parts/player/ScrapingPart.tsx
@@ -110,47 +110,41 @@ export function ScrapingPart(props: ScrapingProps) {
         })}
         ref={listRef}
       >
-        {sourceOrder.map((order) => {
-          const source = sources[order.id];
-          const distance = Math.abs(
-            sourceOrder.findIndex((o) => o.id === order.id) -
-              currentProviderIndex,
-          );
-          return (
-            <div
-              className="transition-opacity duration-100"
-              style={{ opacity: Math.max(0, 1 - distance * 0.3) }}
-              key={order.id}
-            >
-              <ScrapeCard
-                id={order.id}
-                name={source.name}
-                status={source.status}
-                hasChildren={order.children.length > 0}
-                percentage={source.percentage}
-              >
-                <div
-                  className={classNames({
-                    "space-y-6 mt-8": order.children.length > 0,
-                  })}
+        {sourceOrder
+          .filter((order) => order.id === currentSource)
+          .map((order) => {
+            const source = sources[order.id];
+            return (
+              <div className="transition-opacity duration-100" key={order.id}>
+                <ScrapeCard
+                  id={order.id}
+                  name={source.name}
+                  status={source.status}
+                  hasChildren={order.children.length > 0}
+                  percentage={source.percentage}
                 >
-                  {order.children.map((embedId) => {
-                    const embed = sources[embedId];
-                    return (
-                      <ScrapeItem
-                        id={embedId}
-                        name={embed.name}
-                        status={embed.status}
-                        percentage={embed.percentage}
-                        key={embedId}
-                      />
-                    );
-                  })}
-                </div>
-              </ScrapeCard>
-            </div>
-          );
-        })}
+                  <div
+                    className={classNames({
+                      "space-y-6 mt-8": order.children.length > 0,
+                    })}
+                  >
+                    {order.children.map((embedId) => {
+                      const embed = sources[embedId];
+                      return (
+                        <ScrapeItem
+                          id={embedId}
+                          name={embed.name}
+                          status={embed.status}
+                          percentage={embed.percentage}
+                          key={embedId}
+                        />
+                      );
+                    })}
+                  </div>
+                </ScrapeCard>
+              </div>
+            );
+          })}
         <div className="flex gap-3 pb-3">
           <Button
             href="/"
